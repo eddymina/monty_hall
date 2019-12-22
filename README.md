b1c97e5aa5449a5698971c0ab62a51a7cae479ad
Eddy Mina <eddymina@Eddys-MBP-5.fios-router.home>
2019-12-22 13:53:35 -0500

    commit all

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# monty_hall
-# monty_hall
+# Monty Hall Problem 
+
+The Monty Hall problem has always been something that has challenged me intuitively so I decided to tackle it from a pythonic point of view. In this repo I made a simple `Monty_Hall` class with simple code the expresses the unique statistic nature of this problem. Below is a simple explanation of the problem:
+
+```
+Explanation:
+
+Suppose you are on a game show and there are 3 doors. Behind two of the doors are goats and the remainder 3rd door had some gold. There are two rounds to this game. In the first round, you must pick on of the 3 doors as your choice. Here your probability of picking a gold behind a door is 1/3 and a goat is 2/3. Following, your choice, the game show host removes on of the known doors that has a goat behind it. (It is assumed here that the game show host always removes a goat door because he/she knowns whats behind it). Following that you enter the second round where there are two doors that remain:: the door you initially selected and the other mystery door. Should you keep your choice or swap your choice to the other door?
+
+
+Solution:
+
+Because there are only two doors remaining in the second round, it is tempting to think you have a 50/50 chance at selecting the right door in the second round, but it isn't true- you cannot discount the first round. As we agreed before the first round each door has a 1/3 probability. Looking at it another way for a door your select in the first round, no matter what your chance (of success) will remain 1/3. Even if 100 doors were added after, because of your initial circumstances, your probability is 1/3. Going back to the first round, for every other door you select, your success rate is 1/3 and remainder doors are 2/3 (1/3 + 1/3). But you cannot choose door at once! Monty makes that easy by removing one of the doors with a goat. As such, switching your decision is ~2/3 or approximately 2x better than keeping your decision!
+```
+---
+
+#### Using Repo: 
+- `git clone https://github.com/eddymina/monty_hall`
+
+```python
+from monty import Monty_Hall 
+mh=Monty_Hall(niters=2,verbose=True) #set the number of iterations and print results (verbose is False)
+## Set niters>700 to to smooth out noise 
+## Might want to set verbose to false too! 
+mh.compute() #
+mh.plot_res() #compute() is needed prior to plot results 
+```
+```
+--------------------
+Prize location:: 2 | User choice:: 1 | States:: [0 1 0]
+Removing door:: 3
+Swap door::2 | Keep door::1
+Swapping was a good choice
+
+Iter 1---------------------
+Prize location:: 2 | User choice:: 2 | States:: [0 1 0]
+Removing door:: 3
+Swap door::1 | Keep door::2
+Keeping was good choice
+##Computations Done##
+```
+---
+
+#### Methodology 
+```
+1. Randomly choose a prize location and define state. (EX: [0 1 0], prize is at location 2 (index 1))
+2. Remove a door that has a goat and is not the door your selected (obviously)
+3. Allow user to swap and keep choice 
+4. Compute Cumulative Success Rate:
+	For an iteration:	
+		success_keeping == 1 else it is 0 
+		success_swapping == 1 else it is 0 
+
+5. For all iterations sum the successes
+6. Plot Results 
+```
+
+
+
+
---
+++ monty.py
@@ -0,0 +1,125 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import random
+
+class Monty_Hall:
+    """
+    Monty Hall class 
+    """
+    def __init__(self,niters=1000,verbose=False):
+        #TO DO Introduce support for n doors 
+        
+        self.niters= niters #define number of iterations 
+        self.verbose=verbose #print game results per iteration 
+        self.swap_good,self.keep_good=[],[] #success rates 
+        
+    def round1(self):
+        """
+        In the first round
+        you define a prize winning 
+        door index and a index (door) 
+        that the user choose
+        """
+        states = np.array([0,0,0]) #set states to 0 
+        
+        options=[0,1,2]
+        prize_ind = random.choice(options) #set gold door index 
+        states[prize_ind]=1 
+        choice=random.choice(options) #set choice set by user (unrestricted )
+
+        if self.verbose: print('Prize location:: {} | User choice:: {} | States:: {}'.format(prize_ind+1,choice+1,states))    
+        return states,choice
+    
+    def door2remove(self,states,choice):
+        """
+        At the beginning of the second round
+        a door must be removed that is not 
+        the door that the user selected 
+        and is not a prize door. 
+        """
+        same= True
+        while same: #check that choice != door removed 
+            remove=random.choice(np.argwhere(states==0).flatten()) #asserting non prize door selected 
+            if  remove == choice:
+                same=True
+            else:
+                same=False
+        if self.verbose: print('Removing door::',remove+1)
+        return remove
+    
+    
+    def round2_swap(self,remove,choice):
+        """
+        Chooising the index that is not 
+        the door you selected nor the 
+        door that was removed
+        """
+        options=[0,1,2]
+        options.remove(remove)
+        options.remove(choice)
+        return options[0]
+
+    
+    def check(self,selected,states):
+        """
+        Check if the final decision matches
+        the location of the prize door. 
+        """
+        if selected == np.argwhere(states==1).flatten():
+            return True
+        else:
+            return False  
+        
+    def compute(self):
+        """
+        Computer over n number of iterations
+        and see the success rate smoothed 
+        """
+        
+        swap_g,keep_g=0,0
+        for i in range(self.niters):
+            if self.verbose: print('\nIter {}---------------------'.format(i))
+            s,c = self.round1()
+            r=self.door2remove(s,c)
+            sel_swp= self.round2_swap(r,c)
+            sel_keep = c
+
+            if self.verbose: print('Swap door::{} | Keep door::{}'.format(sel_swp+1,sel_keep+1))
+            if self.check(sel_swp,s):
+                if self.verbose: print('Swapping was a good choice')
+                self.swap_good.append(1)
+                self.keep_good.append(0)
+            elif self.check(sel_keep,s):
+                self.keep_good.append(1)
+                self.swap_good.append(0)
+                if self.verbose: print('Keeping was good choice')
+            else:
+                raise ValueError('Something Weird Is going on')
+
+        if self.verbose: print('##Computations Done##')
+            
+    def plot_res(self):
+        """
+        Plotting the cummulative success rate 
+        over the number of games 
+        """
+        plt.figure(figsize=(10,5))
+        plt.xlabel('Iters')
+        plt.ylabel('Cumulative Succes Rate')
+        
+        if len(self.swap_good)==0:
+            raise ValueError('Please Run Monty_Hall(*args).compute() first')
+            
+        self.res_swapping= np.cumsum(self.swap_good)/self.niters
+        plt.plot(np.arange(self.niters),self.res_swapping,label='Swapping Is Good')
+        self.res_keeping= np.cumsum(self.keep_good)/self.niters
+        plt.plot(np.arange(self.niters),self.res_keeping,label='Keeping Is Good')
+        plt.legend()
+        plt.show()
+
+
+if __name__ == '__main__':
+    mh=Monty_Hall(niters=100,verbose=True)
+    mh.compute()
+    mh.plot_res()
+
